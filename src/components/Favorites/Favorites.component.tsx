955e71aa8928d406ab86e41892bacb602b0c64c6
Jelli <julie.nhudo101@gmail.com>
2023-02-17 22:28:26 -0800

    [Favorites] Added store to favorites comp.

--- src/components/Favorites/Favorites.component.tsx
+++ src/components/Favorites/Favorites.component.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { forwardRef } from "react";
 import { useHasMounted } from "../../hooks/useHasMounted";
+import { useFavoritesStore } from "../../store/favorites-store";
 
 type FavoritesProps = {};
+
 export const Favorites = forwardRef<HTMLDivElement, FavoritesProps>(
   ({ ...props }, ref) => {
     const mounted = useHasMounted();
+    const { favoriteTems, addToFavorites, removeFromFavorites } =
+      useFavoritesStore();
 
     if (!mounted) return <>skeleton</>;
 
